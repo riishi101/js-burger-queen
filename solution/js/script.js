56e687ffa514a03552b4e44225658ffb5fc3b2ad
Rishi Raijung <118293792+riishi101@users.noreply.github.com>
2024-12-25 10:18:47 +0530

    Add files via upload

---
+++ solution/js/script.js
@@ -0,0 +1,104 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+// Menu Show
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+// Menu hidden
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLinks = document.querySelectorAll(".nav__link");
+
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
+};
+navLinks.forEach((n) => n.addEventListener("click", linkAction));
+
+/*=============== ADD SHADOW HEADER ===============*/
+const shadowHeader = () => {
+  const header = document.getElementById("header");
+  // Add a class if the scroll offset is greater than 50 of the viewport height
+  window.scrollY >= 50
+    ? header.classList.add("shadow-header")
+    : header.classList.remove("shadow-header");
+};
+window.addEventListener("scroll", shadowHeader);
+
+/*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 350 viewport height, add the show-scroll class
+  window.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
+
+/*=============== ADD TO CART FUNCTIONALITY  ===============*/
+const cartButtons = document.querySelectorAll(".popular__button");
+
+function checkCart() {
+  alert("Sorry we're currently out of business.");
+}
+
+cartButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+    setTimeout(checkCart, 100); // Adds a small delay for better UX
+  });
+});
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+  const scrollY = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionClass = document.querySelector(
+        ".nav__menu a[href*=" + sectionId + "]"
+      );
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionClass.classList.add("active-link");
+    } else {
+      sectionClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 300,
+  //reset: true, // Animations repeat
+});
+
+sr.reveal(".home__data, .footer");
+sr.reveal(".home__dish", { delay: 500, distance: "100px", origin: "bottom" });
+sr.reveal(".home__burger", { delay: 1200, distance: "100px", duration: 1500 });
+sr.reveal(".home__ingredient", { delay: 1600, interval: 100 });
+sr.reveal(".recipe__img, .delivery__img, .contact__image", { origin: "left" });
+sr.reveal(".recipe__data, .delivery__data, .contact__data", {
+  origin: "right",
+});
+sr.reveal(".popular__card", { interval: 100 });
